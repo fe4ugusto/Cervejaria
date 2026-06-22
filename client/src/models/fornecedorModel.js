eddf8227b3f8fe9e7b27f01d9e64a7de573b21cb
Felipe Augusto <augustofelipe807@gmail.com>
2026-06-22 13:14:02 -0300

    chore: remove pasta client desnecessaria

--- client/src/models/fornecedorModel.js
+++
@@ -1,9 +0,0 @@
-/**
- * Modelo de Fornecedores (Fornecedor Model)
- * Define os valores padrão e esquemas para os Fornecedores.
- */
-
-export const DEFAULT_FORNECEDORES = [
-  { id: "1", nome: "HopsBrasil", contato: "contato@hopsbrasil.com", cidade: "São Paulo", ativo: true },
-  { id: "2", nome: "MalteMaster", contato: "malte@master.com.br", cidade: "Blumenau", ativo: true },
-];
